29ffb67857332cf5e271d955c91d693ad134b49d
VerkunderWP <s.kapustin@webpractik.ru>
2022-07-05 10:52:16 +0300

    add repo

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Test from './Test.tsx'
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <Test/>
+        </div>
+    );
+};
+
+export default App;
