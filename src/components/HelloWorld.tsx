747fa402750040a1c21a71b9fcc3147f98c720dc
Kapustin Stepan <s.kapustin@webpractik.ru>
2022-11-08 11:58:05 +0300

    feat(prettier): подключил

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useEffect, useState, Suspense} from 'react';
-import {IDevelopers} from "../types/type";
-import Test from "../assets/resource/image.svg"
-import Icon from "./Icon";
-import './test.sass'
+import React, { FC, useEffect, useState, Suspense } from 'react';
+import { IDevelopers } from '../types/type';
+import Test from '../assets/resource/image.svg';
+import Icon from './Icon';
+import './test.sass';
 const Modal = React.lazy(() => import('./Modal'));
 
 interface IProps {
@@ -10,14 +10,14 @@ interface IProps {
 }
 
 type Todos = {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-}
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+};
 
 function log() {
-    console.log('Tomatos')
+    console.log('Tomatos');
 }
 
 // @ts-ignore
@@ -28,43 +28,44 @@ class User {
     }
 }
 
-const HelloWorld: FC<IProps> = ({text}) => {
+const HelloWorld: FC<IProps> = ({ text }) => {
     const developers: IDevelopers = {
         name: 'Text',
-        age: 17
-    }
+        age: 17,
+    };
 
     const [isOpen, setIsOpen] = useState(false);
 
-    const [todos, setTodos] = useState<Todos>(null)
+    const [todos, setTodos] = useState<Todos>(null);
 
     const getTodos = async () => {
-        const todos = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-            .then(response => response.json())
-        console.log(todos)
-        setTodos(todos)
-    }
+        const todos = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(response =>
+            response.json()
+        );
+        console.log(todos);
+
+        setTodos(todos);
+    };
 
     const isOpenHandler = () => {
-        setIsOpen(!isOpen)
-    }
+        setIsOpen(!isOpen);
+    };
 
     useEffect(() => {
-        getTodos()
+        getTodos();
         User.staticMethod(); // true
-    }, [])
+    }, []);
 
     return (
         <div>
-            <Icon/>
+            <Icon />
             <h1>{text}</h1>
-            <div className='logoTest'>
-            </div>
+            <div className="logoTest"></div>
             {developers.name}, {developers.age}
             <div>
                 <h1>Todos</h1>
-                 {todos && (
-                    <div className='testDiv'>
+                {todos && (
+                    <div className="testDiv">
                         <div>{todos.userId}</div>
                         <div>{todos.id}</div>
                         <div>{todos.title}</div>
@@ -72,19 +73,19 @@ const HelloWorld: FC<IProps> = ({text}) => {
                     </div>
                 )}
             </div>
-
-
             <Test />
-            <a href='lk/example.html'>Go to LK</a>
+            <a href="lk/example.html">Go to LK</a>
             <button onClick={isOpenHandler}>Open Modal</button>
             <Suspense fallback={<div>Loading...</div>}>
-                {isOpen && <Modal
-                    isVisible={isOpen}
-                    title="Modal Title"
-                    content={<p>Add your content here</p>}
-                    footer={<button>Cancel</button>}
-                    onClose={() => setIsOpen(false)}
-                />}
+                {isOpen && (
+                    <Modal
+                        isVisible={isOpen}
+                        title="Modal Title"
+                        content={<p>Add your content here</p>}
+                        footer={<button>Cancel</button>}
+                        onClose={() => setIsOpen(false)}
+                    />
+                )}
             </Suspense>
         </div>
     );
